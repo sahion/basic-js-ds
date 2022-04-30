40387c384d5cf017a40f3f7e7cde985d5dace802
sahion <alexeysuvorov33@gmail.com>
2022-04-30 21:59:10 +0700

    make binary-search tree task, it was hard

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+ const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,39 +8,155 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
+  node = new Node();
+  checkLeft(branch){
+    if (branch.left){
+      return branch.left;
+    }
+    return branch;
+  }
+
+  checkRight(branch){
+    if (branch.right){
+      return branch.right;
+    }
+    return branch;
+  }
+  findPlace(branch, data, parentBranch=branch,position){
+
+    if (typeof this.node.data === 'undefined'){
+      return [branch,'first'];
+    }
+    else if (branch.data>data){
+      let leftBranch = this.checkLeft(branch);
+      if (leftBranch == branch){
+        return [branch,'left']
+      }
+      return this.findPlace(leftBranch,data,branch,'left');
+    } else if (branch.data<data){
+      let rightBranch = this.checkRight(branch);
+      if (rightBranch == branch){
+        return [branch,'right']
+      }
+      this.parentBranch = branch;
+      return this.findPlace(rightBranch,data,branch,'right');
+    } else {
+      return [branch,'exist',parentBranch,position];
+    }
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return (this.node.data) ? this.node : null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    if (!data){return null};
+    let result = this.findPlace(this.node,data);
+    switch (result[1]){
+
+      case 'first':
+        result[0].data = data;
+        break;
+
+      case 'left':
+        result[0].left = new Node(data);
+        break;
+
+      case 'right':
+        result[0].right = new Node(data);
+        break;
+
+      case 'exist':
+        return 'such element already exist';
+        break;
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    let result = this.findPlace(this.node, data);
+    return (result[1]=='exist') ? true : false;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let result = this.findPlace(this.node, data);
+    return (result[1]=='exist') ? result[0] : null;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    let node =  this.findPlace(this.node,data);
+    if (node[1]!='exist') return null;
+
+
+
+    if (node[0].left && node[0].right){
+      debugger;
+      let maxNode = node[0].left;
+      while (this.checkRight(maxNode)!= maxNode){
+        maxNode = maxNode.right; 
+      }
+      this.remove(maxNode.data);
+      node[0].data = maxNode.data;
+
+
+    } else if (node[0].left){
+
+
+      switch (node[3]){
+
+        case ('left'):
+          node[2].left = node[0].left;
+          break;
+
+        case ('right'):
+          node[2].right = node[0].left;
+          break;
+        }
+
+
+    } else if (node[0].right){
+      
+      switch (node[3]){
+
+        case ('left'):
+          node[2].left = node[0].right;
+          break;
+
+        case ('right'):
+          node[2].right = node[0].right;
+          break;
+        }
+
+
+    } else {
+      switch (node[3]){
+
+        case ('left'):
+          node[2].left = null;
+          break;
+
+        case ('right'):
+          node[2].right = null;
+          break;
+      }  
+    }
+    
+    
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let minNode = this.node;
+    while (this.checkLeft(minNode)!= minNode){
+      minNode = minNode.left; 
+    }
+    return minNode.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let maxNode = this.node;
+    while (this.checkRight(maxNode)!= maxNode){
+      maxNode = maxNode.right; 
+    }
+    return maxNode.data;
   }
 }
 
